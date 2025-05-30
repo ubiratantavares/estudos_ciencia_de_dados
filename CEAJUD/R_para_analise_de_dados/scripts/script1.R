25333a26d8ea290f9df3988b48640fc9ea4bc2e2
Ubiratan Tavares <ust1973@gmail.com>
2025-05-30 04:12:14 -0300

    inserindo o script01 na pasta CEAJUD/R_para_analise_de_dados/scripts

---
+++ CEAJUD/R_para_analise_de_dados/scripts/script1.R
@@ -0,0 +1,83 @@
+#-----------------------------------------------------------------------
+# JUSTIÇA 4.0
+# INOVAÇÃO E EFETIVIDADE NA REALIZAÇÃO DA JUSTIÇA PARA TODOS
+# PROJETO DE EXECUÇÃO NACIONAL BRA/20/015
+#
+# R para Análise de Dados
+#
+#                                     Prof. Dr. Walmes M. Zeviani
+#                                     Departamento de Estatística · UFPR
+#-----------------------------------------------------------------------
+
+2 + 2
+
+exp(1)
+
+log10(25) # 2+ 2
+
+# 2+2
+
+2 *
+    2 *
+    2 *
+    5
+
+x = 2
+x <- 2
+
+60 -> x
+
+
+peso <- 83
+altura <- 1.85
+imc <- peso/altura^2
+
+diametro
+
+ls()
+rm(x)
+
+x
+
+rm(altura)
+rm("peso")
+
+assign("x", 2 + 2)
+x <- 2 + 2
+
+exp(x)
+
+women
+
+search()
+
+find("women")
+find("exp")
+
+women <- 100
+
+women
+find("women")
+
+rm(women)
+
+datasets::women
+
+writeLines(R.version.string, con = "/home/walmes/Dropbox/R-versao.txt")
+getwd()
+setwd("~/Downloads")
+
+setwd("~/Downloads/bDMZT/abDMZ/cursors")
+setwd("~/Desktop")
+
+z <- 1234
+
+save.image("file.RData")
+
+# dir(pattern = "*.txt")
+
+writeLines(R.version.string, con = "../R-versao.txt")
+
+
+
+getwd()
