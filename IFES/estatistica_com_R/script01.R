5505f2df3341d263a0566e3b214b09fc3850dd9f
Ubiratan Tavares <ust1973@gmail.com>
2025-06-05 05:01:21 -0300

    implementando mantemética básica

---
+++ IFES/estatistica_com_R/script01.R
@@ -0,0 +1,16 @@
+# Operacoes basicas da matematica
+6 + 3 #soma
+6 - 3 #subtracao
+6 * 3 #multiplicacao
+6 / 3 #divisao
+6 ^ 3 #potenciacao
+6 ** 3 #potenciacao
+sqrt(81) #raiz quadrada
+81 ^ (1/2) #raiz quadrada
+216 ^ (1/3) #raiz cubica
+10 %% 3 #resto da divisao
+log(8) #logaritimo natural de 8
+
+# Solucao das atividades
+(6 + 3 * 5) / 7 # 3
+(9 * 4) ** (2/3) # 10,90272
